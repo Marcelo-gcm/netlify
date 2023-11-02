75c520b4fff8644fd0e143109c5b440021f521c3
Marcelo-gcm <marcelosdegama@gmail.com>
2023-11-02 19:01:23 -0300

    Aqui tem um arquivo em php alem das alterações

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
 
-  if(confirm("Funcionou tudinho ?")){
-    console.log('Funcionou!');
-  }
+    const hamburger = document.querySelector(".hamburger");
+    const menu  = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  
